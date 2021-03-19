a49e4ceb3cf3ad735cdd8d562327b23848e08456
DJ Steinmetz <steinmetz.dj@gmail.com>
2021-03-19 17:55:50 -0500

    fix build 2.0

--- pages/entry/edit/[id].tsx
+++ pages/entry/edit/[id].tsx
@@ -5,7 +5,6 @@ import EditEntryForm from '@/components/edit-entry-form'
 export default function EditEntryPage() {
   return (
     <>
-      <Nav title="Edit" />
       <Container>
         <EditEntryForm />
       </Container>
