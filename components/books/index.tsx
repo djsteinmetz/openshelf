02af21c53313f51161cb34d89cc080756e3c7bff
DJ Steinmetz <steinmetz.dj@gmail.com>
2021-03-24 19:33:00 -0500

    Fix build

--- components/books/index.tsx
+++ components/books/index.tsx
@@ -1,11 +1,7 @@
+import { IBook } from "models/books.interface";
+import React from "react";
+import BookCard from "./book-card";
 
-import { IBook } from 'models/books.interface';
-import React from 'react';
-import Grid from '@material-ui/core/Grid'
-import BookCard from './book-card';
-
-export default function Books({books}) {
-  return (
-      books?.map((b: IBook, i: number) => <BookCard key={i} book={b} />)
-  );
+export default function Books({ books }) {
+  return books?.map((b: IBook, i: number) => <BookCard key={i} book={b} />);
 }
