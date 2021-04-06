9b029e84a2efda6fc34368981c54bd03a534e924
DJ Steinmetz <steinmetz.dj@gmail.com>
2021-04-05 21:37:29 -0500

    Fix unauthorized bug with favorites

--- pages/api/me/favorites/index.ts
+++ pages/api/me/favorites/index.ts
@@ -5,12 +5,15 @@ import { query } from "../../../../lib/db";
 var { verify } = require("jsonwebtoken");
 
 const handler: NextApiHandler = async (_, res) => {
-    const cookie = parse(_.headers.cookie)['bookstr.access_token']
-    try {
-        const decoded = verify(cookie, process.env.NEXT_PUBLIC_API_SECRET);
-        if (_.method === 'GET') {
-            try {
-                const results = await query(`
+  const cookie = parse(_.headers.cookie)['bookstr.access_token']
+  if (!cookie) {
+    return res.status(401).json({'Error': 'Unauthorized'})
+  }
+  try {
+    const decoded = verify(cookie, process.env.NEXT_PUBLIC_API_SECRET);
+    if (_.method === 'GET') {
+      try {
+        const results = await query(`
                 SELECT
                 Users.FullName AS OwnerFullName, 
                 CASE WHEN Users.Verified=1 THEN 'true' ELSE 'false' END AS OwnerVerified,
@@ -40,33 +43,33 @@ const handler: NextApiHandler = async (_, res) => {
                 Books.Title 
               ASC
             `, [decoded.id]);
-                if (results) {
-                    (results as any).forEach((r) => {
-                        r.OwnerVerified = booleanConversion(r.OwnerVerified);
-                    });
-                }
-                return res.status(200).json(results);
-            } catch (e) {
-                res.status(500).json({ message: e.message });
-            }
-
+        if (results) {
+          (results as any).forEach((r) => {
+            r.OwnerVerified = booleanConversion(r.OwnerVerified);
+          });
         }
-        if (_.method === 'POST') {
-            const favs = _.body.favorites.join()
-            const results = await query(`
+        return res.status(200).json(results);
+      } catch (e) {
+        res.status(500).json({ message: e.message });
+      }
+
+    }
+    if (_.method === 'POST') {
+      const favs = _.body.favorites.join()
+      const results = await query(`
                 UPDATE 
                     Users
                 SET 
                     Favorites = ?
                 WHERE 
                     ID = ?`,
-            [favs, decoded.id])
+        [favs, decoded.id])
 
-            return res.status(200).json({ favorites: _.body.favorites })
-        }
-    } catch (err) {
-        return res.status(500).json({ err })
+      return res.status(200).json({ favorites: _.body.favorites })
     }
+  } catch (err) {
+    return res.status(401).json({ message: 'Unauthorized' })
+  }
 };
 
 export default handler;
